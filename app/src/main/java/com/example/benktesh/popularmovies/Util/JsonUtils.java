f0eccb9175c65b5608c506d2b032b99268414be2
Benktesh Sharma <benktesh@yahoo.com>
2018-05-01 22:11:32 -0500

    adds network and jsonUtils
    both incomplete

---
+++ app/src/main/java/com/example/benktesh/popularmovies/Util/JsonUtils.java
@@ -0,0 +1,74 @@
+package com.example.benktesh.popularmovies.Util;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.benktesh.popularmovies.Model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
+    public static Movie parseSandwichJson(String json) {
+        try {
+
+
+            Movie movie = new Movie();
+
+            JSONObject object = new JSONObject(json);
+
+            //name is complex object
+            JSONObject nameObject = object.optJSONObject("name");
+            //sandwich.setMainName(nameObject.optString("mainName", "Not Found"));
+            //sandwich.setAlsoKnownAs(getStringsFromArray(nameObject.optString("alsoKnownAs", "[\"\"]")));
+
+            //sandwich.setPlaceOfOrigin(object.optString("placeOfOrigin", "Unknown"));
+            //sandwich.setDescription(object.optString("description", "Not Found"));
+            //sandwich.setImage(object.getString("image"));
+            //sandwich.setIngredients(getStringsFromArray(object.optString("ingredients", "[\"\"]")));
+
+            return movie;
+        } catch (Exception ex) {
+            Log.e(TAG + "parseSandwichJson", "Could not parse json " + json);
+            return null;
+        }
+    }
+
+
+    /**
+     * This method parses a json string to a list of string
+     *
+     * @param - String stringArray representation of json array
+     * @return List<String> list of string
+     * @throws JSONException
+     */
+    @NonNull
+    private static List<String> getStringsFromArray(String stringArray) {
+
+        List<String> list = new ArrayList<String>();
+        try {
+
+
+            JSONArray jsonarray = null;
+
+            jsonarray = new JSONArray(stringArray);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                list.add(jsonarray.optString(i, "Not Found"));
+            }
+            return list;
+        } catch (Exception ex)
+
+        {
+            Log.e(TAG + "getStringFromArray" + stringArray, ex.toString());
+
+        }
+        return list;
+    }
+}
---
+++ app/src/main/java/com/example/benktesh/popularmovies/Util/NetworkUtilities.java
@@ -0,0 +1,73 @@
+package com.example.benktesh.popularmovies.Util;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by Benktesh on 5/1/18.
+ * Some of the resource for this file were created from Udacity provided content for the students in Google Challenge Scholar's Exercise 2.
+ */
+
+public class NetworkUtilities {
+
+    final static String BASE_URL = "http://image.tmdb.org/t/p/";
+
+    final static String PARAM_QUERY = "q";
+
+    //widht of the poster
+    final static String WIDTH = "w185";
+
+    /**
+     * Builds the URL to fetch poster image.
+     *
+     * @param poster The file for poster.
+     * @return The URL to use to query the GitHub.
+     */
+    public static URL buildPosterUrl(String poster) {
+
+        Uri builtUri = Uri.parse(BASE_URL).buildUpon().appendPath(WIDTH).appendPath("/n").appendPath(poster)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
