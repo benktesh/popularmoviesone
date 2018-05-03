8f21f19bf7fc8e52da884847682f39ef6747060f
Benktesh Sharma <benktesh@yahoo.com>
2018-05-03 16:23:30 -0500

    a simple applicaiton with recycle view and adapter

---
+++ app/src/main/java/com/example/benktesh/popularmovies/MovieAdapter.java
@@ -0,0 +1,77 @@
+package com.example.benktesh.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Benktesh on 5/2/18.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private static final String TAG = MovieAdapter.class.getSimpleName();
+    private int mNumberItems;
+    private static int viewHolderCount;
+
+    final private ListItemClickListener mOnClickListener;
+
+
+    public interface ListItemClickListener {
+        void OnListItemClick(int clickedItemIndex);
+    }
+
+
+    public MovieAdapter(int numberItems, ListItemClickListener listener) {
+        mNumberItems = numberItems;
+        viewHolderCount = 0;
+        mOnClickListener = listener;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        MovieViewHolder viewHolder = new MovieViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView listMovieItemView;
+
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+
+            listMovieItemView = (TextView) itemView.findViewById(R.id.tv_item_number);
+            itemView.setOnClickListener(this);
+        }
+
+        void bind(int listIndex) {
+            listMovieItemView.setText(String.valueOf(listIndex));
+        }
+
+        @Override
+        public void onClick(View view) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.OnListItemClick(clickedPosition);
+        }
+    }
+
+}
